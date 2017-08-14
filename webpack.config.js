5031e5680c1c3ea8ace596ca156e3e92a706b915
Nathanael Smith <nathanael@ciedigital.com>
2017-08-14 12:54:29 -0700

    Install on-build plugin and call fabricator

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,47 @@
 const path = require('path');
 const webpack = require('webpack');
+const assembler = require('fabricator-assemble');
+const WebpackOnBuildPlugin = require('on-build-webpack');
 
+const config = {
+  dev: process.env.NODE_ENV === "development",
+  styles: {
+    browsers: 'last 1 version',
+    fabricator: {
+      src: 'src/assets/fabricator/styles/fabricator.scss',
+      dest: 'dist/assets/fabricator/styles',
+      watch: 'src/assets/fabricator/styles/**/*.scss',
+    },
+    toolkit: {
+      src: 'src/assets/toolkit/styles/toolkit.scss',
+      dest: 'dist/assets/toolkit/styles',
+      watch: 'src/assets/toolkit/styles/**/*.scss',
+    },
+  },
+  scripts: {
+    fabricator: {
+      src: './src/assets/fabricator/scripts/fabricator.js',
+      dest: 'dist/assets/fabricator/scripts',
+      watch: 'src/assets/fabricator/scripts/**/*',
+    },
+    toolkit: {
+      src: './src/assets/toolkit/scripts/toolkit.js',
+      dest: 'dist/assets/toolkit/scripts',
+      watch: 'src/assets/toolkit/scripts/**/*',
+    },
+  },
+  images: {
+    toolkit: {
+      src: ['src/assets/toolkit/images/**/*', 'src/favicon.ico'],
+      dest: 'dist/assets/toolkit/images',
+      watch: 'src/assets/toolkit/images/**/*',
+    },
+  },
+  templates: {
+    watch: 'src/**/*.{html,md,json,yml}',
+  },
+  dest: 'dist',
+};
 
 /**
  * Define plugins based on environment
@@ -10,14 +51,18 @@ const webpack = require('webpack');
 function getPlugins(isDev) {
 
   const plugins = [
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({}),
+    new WebpackOnBuildPlugin(function() {
+      assembler({
+        logErrors: config.dev,
+        dest: config.dest,
+      });
+    })
   ];
 
   if (isDev) {
     plugins.push(new webpack.NoErrorsPlugin());
   } else {
-    plugins.push(new webpack.optimize.DedupePlugin());
     plugins.push(new webpack.optimize.UglifyJsPlugin({
       minimize: true,
       sourceMap: false,
@@ -53,24 +98,18 @@ function getLoaders() {
 
 }
 
-
-module.exports = (config) => {
-  return {
-    entry: {
-      'fabricator/scripts/f': config.scripts.fabricator.src,
-      'toolkit/scripts/toolkit': config.scripts.toolkit.src,
-    },
-    output: {
-      path: path.resolve(__dirname, config.dest, 'assets'),
-      filename: '[name].js',
-    },
-    devtool: 'source-map',
-    resolve: {
-      extensions: ['', '.js'],
-    },
-    plugins: getPlugins(config.dev),
-    module: {
-      loaders: getLoaders(),
-    },
-  };
+module.exports = {
+  entry: {
+    'fabricator/scripts/f': config.scripts.fabricator.src,
+    'toolkit/scripts/toolkit': config.scripts.toolkit.src,
+  },
+  output: {
+    path: path.resolve(__dirname, config.dest, 'assets'),
+    filename: '[name].js',
+  },
+  devtool: 'source-map',
+  plugins: getPlugins(config.dev),
+  module: {
+    loaders: getLoaders(),
+  }
 };
