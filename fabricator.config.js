35552802604c212c1d3e4a972aba66b88e1c1da1
Nathanael Smith <nathanaelnsmith@gmail.com>
2017-08-14 21:29:27 -0700

    Parse context for asset filename

--- fabricator.config.js
+++ fabricator.config.js
@@ -20,26 +20,28 @@ const fabricator = {
     dest: 'dist'
   },
   assembler: function (stats) {
-    const assets = stats.toJson().assets;
-    const assetMap = assets.reduce(function (map, values) {
-      const extension = values.name.split('.').pop();
-      const chunkName = values.chunkNames[0];
-      if (chunkName) {
-        if (!map.hasOwnProperty(chunkName)) {
-          map[chunkName] = {};
-        }
-        map[chunkName][extension] = values.name;
-      }
-      return map;
-    }, {});
-    console.log(assetMap);
+    const assetMap = createHashMap(stats.toJson().assets);
     console.log('Assembling templates.');
     assembler({
       logErrors: fabricator.config.dev,
       dest: fabricator.config.dest,
-      assetMap
+      buildData: assetMap
     });
   }
+}
+
+function createHashMap (assets) {
+  return assets.reduce(function (map, values) {
+    const extension = values.name.split('.').pop();
+    const chunkName = values.chunkNames[0];
+    if (chunkName) {
+      if (!map.hasOwnProperty(chunkName)) {
+        map[chunkName] = {};
+      }
+      map[chunkName][extension] = values.name;
+    }
+    return map;
+  }, {});
 }
 
 module.exports = fabricator;
