2be5f1756eb91ed9720c2f8a23511a7a7a103ff0
Nathanael Smith <nathanaelnsmith@gmail.com>
2017-08-17 20:37:19 -0700

    Integrate foundation classes

--- src/assets/fabricator/scripts/fabricator.js
+++ src/assets/fabricator/scripts/fabricator.js
@@ -1,11 +1,13 @@
+'use strict';
+
 require('./prism');
-import '../styles/fabricator.scss';
+require('../styles/fabricator.scss');
 
 /**
  * Global `fabricator` object
  * @namespace
  */
-const fabricator = window.fabricator = {};
+var fabricator = window.fabricator = {};
 
 
 /**
@@ -13,13 +15,13 @@ const fabricator = window.fabricator = {};
  * @type {Object}
  */
 fabricator.options = {
-  toggles: {
-    labels: true,
-    notes: true,
-    code: false,
-  },
-  menu: false,
-  mq: '(min-width: 60em)',
+	toggles: {
+		labels: true,
+		notes: true,
+		code: false
+	},
+	menu: false,
+	mq: '(min-width: 60em)'
 };
 
 // open menu by default if large screen
@@ -32,20 +34,20 @@ fabricator.options.menu = window.matchMedia(fabricator.options.mq).matches;
 fabricator.test = {};
 
 // test for sessionStorage
-fabricator.test.sessionStorage = (() => {
-  const test = '_f';
-  try {
-    sessionStorage.setItem(test, test);
-    sessionStorage.removeItem(test);
-    return true;
-  } catch (e) {
-    return false;
-  }
-})();
+fabricator.test.sessionStorage = (function () {
+	var test = '_f';
+	try {
+		sessionStorage.setItem(test, test);
+		sessionStorage.removeItem(test);
+		return true;
+	} catch(e) {
+		return false;
+	}
+}());
 
 // create storage object if it doesn't exist; store options
 if (fabricator.test.sessionStorage) {
-  sessionStorage.fabricator = sessionStorage.fabricator || JSON.stringify(fabricator.options);
+	sessionStorage.fabricator = sessionStorage.fabricator || JSON.stringify(fabricator.options);
 }
 
 
@@ -54,10 +56,10 @@ if (fabricator.test.sessionStorage) {
  * @type {Object}
  */
 fabricator.dom = {
-  root: document.querySelector('html'),
-  primaryMenu: document.querySelector('.f-menu'),
-  menuItems: document.querySelectorAll('.f-menu li a'),
-  menuToggle: document.querySelector('.f-menu-toggle'),
+	root: document.querySelector('html'),
+	primaryMenu: document.querySelector('.f-menu'),
+	menuItems: document.querySelectorAll('.menu li'),
+	menuToggle: document.querySelector('.f-menu-toggle')
 };
 
 
@@ -65,69 +67,101 @@ fabricator.dom = {
  * Get current option values from session storage
  * @return {Object}
  */
-fabricator.getOptions = () => {
-  return (fabricator.test.sessionStorage) ? JSON.parse(sessionStorage.fabricator) : fabricator.options;
+fabricator.getOptions = function () {
+	return (fabricator.test.sessionStorage) ? JSON.parse(sessionStorage.fabricator) : fabricator.options;
 };
 
 
 /**
  * Build color chips
  */
-fabricator.buildColorChips = () => {
+fabricator.buildColorChips = function () {
 
-  const chips = document.querySelectorAll('.f-color-chip');
+	var chips = document.querySelectorAll('.f-color-chip'),
+		color;
 
-  for (let i = chips.length - 1; i >= 0; i--) {
-    const color = chips[i].querySelector('.f-color-chip__color').innerHTML;
-    chips[i].style.borderTopColor = color;
-    chips[i].style.borderBottomColor = color;
-  }
+	for (var i = chips.length - 1; i >= 0; i--) {
+		color = chips[i].querySelector('.f-color-chip__color').innerHTML;
+		chips[i].style.borderTopColor = color;
+		chips[i].style.borderBottomColor = color;
+	}
 
-  return fabricator;
+	return this;
 
 };
 
 
 /**
- * Add `f-active` class to active menu item
+ * Add `is-active` class to active menu item
  */
-fabricator.setActiveItem = () => {
+fabricator.setActiveItem = function () {
 
-  /**
-   * Match the window location with the menu item, set menu item as active
-   */
-  const setActive = () => {
+	/**
+	 * @return {Array} Sorted array of menu item 'ids'
+	 */
+	var parsedItems = function () {
 
-    // get current file and hash without first slash
-    const loc = (window.location.pathname + window.location.hash);
-    const current = loc.replace(/(^\/)([^#]+)?(#[\w\-\.]+)?$/ig, (match, slash, file, hash) => {
-      return (file || '') + (hash || '').split('.')[0];
-    }) || 'index.html';
+		var items = [],
+			id, href;
+
+		for (var i = fabricator.dom.menuItems.length - 1; i >= 0; i--) {
+
+			// remove active class from items
+			fabricator.dom.menuItems[i].classList.remove('is-active');
+
+			// get item href
+			href = fabricator.dom.menuItems[i].children[0].getAttribute('href');
+
+			// get id
+			if (href.indexOf('#') > -1) {
+				id = href.split('#').pop();
+			} else {
+				id = href.split('/').pop().replace(/\.[^/.]+$/, '');
+			}
+
+			items.push(id);
+
+		}
+
+		return items.reverse();
+
+	};
 
 
-    // find the current section in the items array
-    for (let i = fabricator.dom.menuItems.length - 1; i >= 0; i--) {
+	/**
+	 * Match the 'id' in the window location with the menu item, set menu item as active
+	 */
+	var setActive = function () {
 
-      const item = fabricator.dom.menuItems[i];
+		var href = window.location.href,
+			items = parsedItems(),
+			id, index;
 
-      // get item href without first slash
-      const href = item.getAttribute('href').replace(/^\//g, '');
+		// get window 'id'
+		if (href.indexOf('#') > -1) {
+			id = window.location.hash.replace('#', '');
+		} else {
+			id = window.location.pathname.split('/').pop().replace(/\.[^/.]+$/, '');
+		}
 
-      if (href === current) {
-        item.classList.add('f-active');
-      } else {
-        item.classList.remove('f-active');
-      }
+		// In case the first menu item isn't the index page.
+		if (id === '') {
+			id = 'index';
+		}
 
-    }
+		// find the window id in the items array
+		index = (items.indexOf(id) > -1) ? items.indexOf(id) : 0;
 
-  };
+		// set the matched item as active
+		fabricator.dom.menuItems[index].classList.add('is-active');
 
-  window.addEventListener('hashchange', setActive);
+	};
 
-  setActive();
+	window.addEventListener('hashchange', setActive);
 
-  return fabricator;
+	setActive();
+
+	return this;
 
 };
 
@@ -136,46 +170,40 @@ fabricator.setActiveItem = () => {
  * Click handler to primary menu toggle
  * @return {Object} fabricator
  */
-fabricator.menuToggle = () => {
+fabricator.menuToggle = function () {
 
-  // shortcut menu DOM
-  const toggle = fabricator.dom.menuToggle;
-  const options = fabricator.getOptions();
+	// shortcut menu DOM
+	var toggle = fabricator.dom.menuToggle;
 
-  // toggle classes on certain elements
-  const toggleClasses = () => {
-    options.menu = !fabricator.dom.root.classList.contains('f-menu-active');
-    fabricator.dom.root.classList.toggle('f-menu-active');
+	var options = fabricator.getOptions();
 
-    if (fabricator.test.sessionStorage) {
-      sessionStorage.setItem('fabricator', JSON.stringify(options));
-    }
-  };
+	// toggle classes on certain elements
+	var toggleClasses = function () {
+		options.menu = !fabricator.dom.root.classList.contains('f-menu-active');
+		fabricator.dom.root.classList.toggle('f-menu-active');
 
-  // toggle classes on ctrl + m press
-  document.onkeydown = (e) => {
-    if (e.ctrlKey && e.keyCode === 'M'.charCodeAt(0)) {
-      toggleClasses();
-    }
-  };
+		if (fabricator.test.sessionStorage) {
+			sessionStorage.setItem('fabricator', JSON.stringify(options));
+		}
+	};
 
-  // toggle classes on click
-  toggle.addEventListener('click', () => {
-    toggleClasses();
-  });
+	// toggle classes on click
+	// toggle.addEventListener('click', function () {
+	// 	toggleClasses();
+	// });
 
-  // close menu when clicking on item (for collapsed menu view)
-  const closeMenu = () => {
-    if (!window.matchMedia(fabricator.options.mq).matches) {
-      toggleClasses();
-    }
-  };
+	// close menu when clicking on item (for collapsed menu view)
+	var closeMenu = function () {
+		if (!window.matchMedia(fabricator.options.mq).matches) {
+			toggleClasses();
+		}
+	};
 
-  for (let i = 0; i < fabricator.dom.menuItems.length; i++) {
-    fabricator.dom.menuItems[i].addEventListener('click', closeMenu);
-  }
+	for (var i = 0; i < fabricator.dom.menuItems.length; i++) {
+		fabricator.dom.menuItems[i].addEventListener('click', closeMenu);
+	}
 
-  return fabricator;
+	return this;
 
 };
 
@@ -184,68 +212,71 @@ fabricator.menuToggle = () => {
  * Handler for preview and code toggles
  * @return {Object} fabricator
  */
-fabricator.allItemsToggles = () => {
+fabricator.allItemsToggles = function () {
 
-  const itemCache = {
-    labels: document.querySelectorAll('[data-f-toggle="labels"]'),
-    notes: document.querySelectorAll('[data-f-toggle="notes"]'),
-    code: document.querySelectorAll('[data-f-toggle="code"]'),
-  };
+	var items = {
+		labels: document.querySelectorAll('[data-f-toggle="labels"]'),
+		notes: document.querySelectorAll('[data-f-toggle="notes"]'),
+		code: document.querySelectorAll('[data-f-toggle="code"]')
+	};
 
-  const toggleAllControls = document.querySelectorAll('.f-controls [data-f-toggle-control]');
-  const options = fabricator.getOptions();
+	var toggleAllControls = document.querySelectorAll('.f-controls [data-f-toggle-control]');
 
-  // toggle all
-  const toggleAllItems = (type, value) => {
+	var options = fabricator.getOptions();
 
-    const button = document.querySelector(`.f-controls [data-f-toggle-control=${type}]`);
-    const items = itemCache[type];
+	// toggle all
+	var toggleAllItems = function (type, value) {
 
-    for (let i = 0; i < items.length; i++) {
-      if (value) {
-        items[i].classList.remove('f-item-hidden');
-      } else {
-        items[i].classList.add('f-item-hidden');
-      }
-    }
+		var button = document.querySelector('.f-controls [data-f-toggle-control=' + type + ']'),
+			_items = items[type];
 
-    // toggle styles
-    if (value) {
-      button.classList.add('f-active');
-    } else {
-      button.classList.remove('f-active');
-    }
+		for (var i = 0; i < _items.length; i++) {
+			if (value) {
+				_items[i].classList.remove('f-item-hidden');
+			} else {
+				_items[i].classList.add('f-item-hidden');
+			}
+		}
 
-    // update options
-    options.toggles[type] = value;
+		// toggle styles
+		if (value) {
+			button.classList.add('f-active');
+		} else {
+			button.classList.remove('f-active');
+		}
 
-    if (fabricator.test.sessionStorage) {
-      sessionStorage.setItem('fabricator', JSON.stringify(options));
-    }
+		// update options
+		options.toggles[type] = value;
 
-  };
+		if (fabricator.test.sessionStorage) {
+			sessionStorage.setItem('fabricator', JSON.stringify(options));
+		}
 
-  for (let i = 0; i < toggleAllControls.length; i++) {
+	};
 
-    toggleAllControls[i].addEventListener('click', (e) => {
+	for (var i = 0; i < toggleAllControls.length; i++) {
 
-      // extract info from target node
-      const type = e.currentTarget.getAttribute('data-f-toggle-control');
-      const value = e.currentTarget.className.indexOf('f-active') < 0;
+		toggleAllControls[i].addEventListener('click', function (e) {
 
-      // toggle the items
-      toggleAllItems(type, value);
+			// extract info from target node
+			var type = e.currentTarget.getAttribute('data-f-toggle-control'),
+				value = e.currentTarget.className.indexOf('f-active') < 0;
 
-    });
+			// toggle the items
+			toggleAllItems(type, value);
 
-  }
+		});
 
-  // persist toggle options from page to page
-  Object.keys(options.toggles).forEach((key) => {
-    toggleAllItems(key, options.toggles[key]);
-  });
+	}
 
-  return fabricator;
+	// persist toggle options from page to page
+	for (var toggle in options.toggles) {
+		if (options.toggles.hasOwnProperty(toggle)) {
+			toggleAllItems(toggle, options.toggles[toggle]);
+		}
+	}
+
+	return this;
 
 };
 
@@ -253,22 +284,23 @@ fabricator.allItemsToggles = () => {
 /**
  * Handler for single item code toggling
  */
-fabricator.singleItemToggle = () => {
+fabricator.singleItemToggle = function () {
 
-  const itemToggleSingle = document.querySelectorAll('.f-item-group [data-f-toggle-control]');
+	var itemToggleSingle = document.querySelectorAll('.f-item-group [data-f-toggle-control]');
 
-  // toggle single
-  const toggleSingleItemCode = (e) => {
-    const group = e.currentTarget.parentNode.parentNode.parentNode;
-    const type = e.currentTarget.getAttribute('data-f-toggle-control');
-    group.querySelector(`[data-f-toggle=${type}]`).classList.toggle('f-item-hidden');
-  };
+	// toggle single
+	var toggleSingleItemCode = function (e) {
+		var group = this.parentNode.parentNode.parentNode,
+			type = e.currentTarget.getAttribute('data-f-toggle-control');
 
-  for (let i = 0; i < itemToggleSingle.length; i++) {
-    itemToggleSingle[i].addEventListener('click', toggleSingleItemCode);
-  }
+		group.querySelector('[data-f-toggle=' + type + ']').classList.toggle('f-item-hidden');
+	};
 
-  return fabricator;
+	for (var i = 0; i < itemToggleSingle.length; i++) {
+		itemToggleSingle[i].addEventListener('click', toggleSingleItemCode);
+	}
+
+	return this;
 
 };
 
@@ -276,21 +308,21 @@ fabricator.singleItemToggle = () => {
 /**
  * Automatically select code when code block is clicked
  */
-fabricator.bindCodeAutoSelect = () => {
+fabricator.bindCodeAutoSelect = function () {
 
-  const codeBlocks = document.querySelectorAll('.f-item-code');
+	var codeBlocks = document.querySelectorAll('.f-item-code');
 
-  const select = (block) => {
-    const selection = window.getSelection();
-    const range = document.createRange();
-    range.selectNodeContents(block.querySelector('code'));
-    selection.removeAllRanges();
-    selection.addRange(range);
-  };
+	var select = function (block) {
+		var selection = window.getSelection();
+		var range = document.createRange();
+		range.selectNodeContents(block.querySelector('code'));
+		selection.removeAllRanges();
+		selection.addRange(range);
+	};
 
-  for (let i = codeBlocks.length - 1; i >= 0; i--) {
-    codeBlocks[i].addEventListener('click', select.bind(this, codeBlocks[i]));
-  }
+	for (var i = codeBlocks.length - 1; i >= 0; i--) {
+		codeBlocks[i].addEventListener('click', select.bind(this, codeBlocks[i]));
+	}
 
 };
 
@@ -299,30 +331,30 @@ fabricator.bindCodeAutoSelect = () => {
  * Open/Close menu based on session var.
  * Also attach a media query listener to close the menu when resizing to smaller screen.
  */
-fabricator.setInitialMenuState = () => {
+fabricator.setInitialMenuState = function () {
 
-  // root element
-  const root = document.querySelector('html');
+	// root element
+	var root = document.querySelector('html');
 
-  const mq = window.matchMedia(fabricator.options.mq);
+	var mq = window.matchMedia(fabricator.options.mq);
 
-  // if small screen
-  const mediaChangeHandler = (list) => {
-    if (!list.matches) {
-      root.classList.remove('f-menu-active');
-    } else {
-      if (fabricator.getOptions().menu) {
-        root.classList.add('f-menu-active');
-      } else {
-        root.classList.remove('f-menu-active');
-      }
-    }
-  };
+	// if small screen
+	var mediaChangeHandler = function (list) {
+		if (!list.matches) {
+			root.classList.remove('f-menu-active');
+		} else {
+			if (fabricator.getOptions().menu) {
+				root.classList.add('f-menu-active');
+			} else {
+				root.classList.remove('f-menu-active');
+			}
+		}
+	};
 
-  mq.addListener(mediaChangeHandler);
-  mediaChangeHandler(mq);
+	mq.addListener(mediaChangeHandler);
+	mediaChangeHandler(mq);
 
-  return fabricator;
+	return this;
 
 };
 
@@ -330,11 +362,16 @@ fabricator.setInitialMenuState = () => {
 /**
  * Initialization
  */
-fabricator
- .setInitialMenuState()
- .menuToggle()
- .allItemsToggles()
- .singleItemToggle()
- .buildColorChips()
- .setActiveItem()
- .bindCodeAutoSelect();
+(function () {
+
+	// invoke
+	fabricator
+		.setInitialMenuState()
+		.menuToggle()
+		.allItemsToggles()
+		.singleItemToggle()
+		.buildColorChips()
+		.setActiveItem()
+		.bindCodeAutoSelect();
+
+}());
